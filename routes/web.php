7205f88a8209cb33390eb1bd781a27cc66921253
Bart Verhaegh <bart@rozenlicht.nl>
2025-08-20 16:41:06 +0200

    Fix: Domains

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,16 @@ Route::get('/', function () {
     return redirect()->away('https://www.tue.nl/en/research/research-labs/multiscale-lab');
 });
 
-Route::get('/login', function () {
-    return redirect()->route('filament.admin.auth.login');
-})->name('login');
-
 
 $domain = config('app.domain');
 
 $routeDefinition = function () {
+    Route::get('/', function () {
+        return redirect()->route('filament.admin.auth.login');
+    });
+    Route::get('/login', function () {
+        return redirect()->route('filament.admin.auth.login');
+    })->name('login');
     Route::get('/qr-code/{containerId}', [QrCodeController::class, 'show'])->name('qr-code.show');
 };
 
