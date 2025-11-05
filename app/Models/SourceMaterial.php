e53f724e85e75361be033b099f2201fcb7cac4b0
Bart Verhaegh <bart@rozenlicht.nl>
2025-11-05 11:28:25 +0100

    Fix: BASIS source mat layout

--- app/Models/SourceMaterial.php
+++ app/Models/SourceMaterial.php
@@ -28,6 +28,26 @@ class SourceMaterial extends Model
         'properties' => 'array',
     ];
 
+    // While saving, if the composition seems to be a string containing valid JSON, parse it into an array
+    public function setCompositionAttribute($value)
+    {
+        if (is_string($value) && json_decode($value, true) !== null) {
+            $this->attributes['composition'] = json_decode($value, true);
+        } else {
+            $this->attributes['composition'] = $value;
+        }
+    }
+
+    // While saving, if the properties seems to be a string containing valid JSON, parse it into an array
+    public function setPropertiesAttribute($value)
+    {
+        if (is_string($value) && json_decode($value, true) !== null) {
+            $this->attributes['properties'] = json_decode($value, true);
+        } else {
+            $this->attributes['properties'] = $value;
+        }
+    }
+    
     public function samples()
     {
         return $this->hasMany(Sample::class);
